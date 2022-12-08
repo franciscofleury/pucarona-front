afe7e5dea27c9978157d056d7fed8e685b537cda
Francisco Meirelles Fleury <franmeifleury@gmail.com>
2022-12-08 18:30:16 -0300

    my setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { LoginBoxComponent } from './login-box/login-box.component';
+import { LoginContainerComponent } from './login-container/login-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    LoginButtonComponent,
+    LoginBoxComponent,
+    LoginContainerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "home", component: HomePageComponent },
+      { path: "login", component: LoginPageComponent },
+      { path: "", redirectTo: "/home", pathMatch: "full" }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
